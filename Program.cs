93f986e255616a9bd89920dd736612a6b6ee4503
Fletcher Moore <154636617+FletcherJMoore@users.noreply.github.com>
2024-08-13 17:36:34 -0500

    Adding DELETE endpoint
    Added an endpoint for deleting campsites

--- Program.cs
+++ Program.cs
@@ -43,6 +43,26 @@ app.MapGet("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
     return db.Campsites.Include(c => c.CampsiteType).Single(c => c.Id == id);
 });
 
+app.MapPost("/api/campsites", (CreekRiverDbContext db, Campsite campsite) =>
+{
+    db.Campsites.Add(campsite);
+    db.SaveChanges();
+    return Results.Created($"/api/campsites/{campsite.Id}", campsite);
+});
+
+app.MapDelete("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
+{
+    Campsite campsite = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsite == null)
+    {
+        return Results.NotFound();
+    }
+    db.Campsites.Remove(campsite);
+    db.SaveChanges();
+    return Results.NoContent();
+
+});
+
 app.Run();
 
 
